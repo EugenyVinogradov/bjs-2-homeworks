c5cd59e79bcc42e8d05b10ed2d2c5ed1f84320d3
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-09-15 11:42:51 +0300

    solution task "arrays"

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  result = false;
+  if (arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i])) {
+    result = true;
+  }
+return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(element => element >= 0 && element % 3 === 0).map(element => element * 10);
   return resultArr; // array
 }
+
