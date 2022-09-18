3711b86a45cf577a5a89969677ac27b28469ecb1
Виноградов Евгений <Vinogradov_e_v@rambler.ru>
2022-09-18 12:52:54 +0300

    task objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...args) {
+  if(this.marks === undefined){ 
+    this.marks = [...args];
+    } else {
+      this.marks.push(...args);
+    }
+}
+Student.prototype.getAverage = function () {
+  if(this.marks === undefined){ 
+    return "У студента нет оценок";
+    } else {
+      let sum =0;
+      for (let mark of this.marks) {
+        sum = sum + mark;
+      }
+      return sum/this.marks.length;
+    }
+}
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
