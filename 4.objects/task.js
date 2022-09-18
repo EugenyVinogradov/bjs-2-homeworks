f87f0cda671cf1485967e1343382e82815fd99a1
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-09-18 20:29:01 +0300

    Edit README`S files

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,6 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -27,13 +26,12 @@ Student.prototype.addMarks = function (...args) {
 Student.prototype.getAverage = function () {
   if(this.marks === undefined){ 
     return "У студента нет оценок";
-    } else {
-      let sum =0;
-      for (let mark of this.marks) {
-        sum = sum + mark;
-      }
-      return sum/this.marks.length;
     }
+    let sum =0;
+    for (let mark of this.marks) {
+      sum = sum + mark;
+    }
+    return sum/this.marks.length;  
 }
 Student.prototype.exclude = function (reason) {
   delete this.subject;
